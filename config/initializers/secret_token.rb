aac4cfcb3949f197de4e45013311917f25280b4d
Blenda <bdefante@gmail.com>
2012-10-09 09:43:52 -0300

    Inicio do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sistemacg::Application.config.secret_token = '541e888feaaedf231c1a560643eb93ee8c9ed281812fba05fa937bc0fb3c3847899b02e2097d730a0de3b42d145991c5ac417e476af46e0aa0e7b0d63e28ce5d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Sistemacg::Application.config.session_store :cookie_store, key: '_sistemacg_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Sistemacg::Application.config.session_store :active_record_store
